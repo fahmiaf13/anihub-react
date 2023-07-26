69b4942792ce4b0c5335ec510e49cdf3044bb1a1
Fahmi Achmad <fahmiachmadf13@gmail.com>
2023-07-26 16:53:06 +0700

    feat: replace png to webp

--- src/assets/images/hero/image.ts
+++ src/assets/images/hero/image.ts
@@ -1,17 +1,17 @@
-import firstHeroImg from "@/assets/images/hero/1.png";
-import secondHeroImg from "@/assets/images/hero/2.png";
-import thirdHeroImg from "@/assets/images/hero/3.png";
-import fourthHeroImg from "@/assets/images/hero/4.png";
-import fifthHeroImg from "@/assets/images/hero/5.png";
-import sixthHeroImg from "@/assets/images/hero/6.png";
-import seventhHeroImg from "@/assets/images/hero/7.png";
-import eighthHeroImg from "@/assets/images/hero/8.png";
-import ninthHeroImg from "@/assets/images/hero/9.png";
-import tenthHeroImg from "@/assets/images/hero/10.png";
-import eleventhHeroImg from "@/assets/images/hero/11.png";
-import twelvethHeroImg from "@/assets/images/hero/12.png";
-import thirteenthHeroImg from "@/assets/images/hero/13.png";
-import fourteenthHeroImg from "@/assets/images/hero/14.png";
+import firstHeroImg from "@/assets/images/hero/1.webp";
+import secondHeroImg from "@/assets/images/hero/2.webp";
+import thirdHeroImg from "@/assets/images/hero/3.webp";
+import fourthHeroImg from "@/assets/images/hero/4.webp";
+import fifthHeroImg from "@/assets/images/hero/5.webp";
+import sixthHeroImg from "@/assets/images/hero/6.webp";
+import seventhHeroImg from "@/assets/images/hero/7.webp";
+import eighthHeroImg from "@/assets/images/hero/8.webp";
+import ninthHeroImg from "@/assets/images/hero/9.webp";
+import tenthHeroImg from "@/assets/images/hero/10.webp";
+import eleventhHeroImg from "@/assets/images/hero/11.webp";
+import twelvethHeroImg from "@/assets/images/hero/12.webp";
+import thirteenthHeroImg from "@/assets/images/hero/13.webp";
+import fourteenthHeroImg from "@/assets/images/hero/14.webp";
 
 interface IHeroImg {
   id: number;
